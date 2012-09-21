accf80c2f881d6b60f5bb7e5f2f6e348ef7b4101
Boris Hekele <hekele@parliamentwatch.org>
2012-09-21 21:31:05 +0200

    css & sprite & js settings to the profiles

--- sites/all/themes/custom/abgeordnetenwatch/js/scripts.js
+++ sites/all/themes/custom/abgeordnetenwatch/js/scripts.js
@@ -1,4 +1,16 @@
+
+//// Report Link
 jQuery(document).ready(function() {
+
+   jQuery("a.report_message").click(function () {
+		if(!confirm('Soll sich ein Moderator Frage und Antwort angeschauen?')){
+			return false;
+		}
+		var message_id = jQuery(this).attr('id').substr(jQuery(this).attr('id').indexOf('_') + 1);
+		var url = 'http://mod.parliamentwatch.org/piraten/api/message/' + message_id + '/report';
+		jQuery.get(url);
+		alert('Vielen Dank! Ein Moderator wird sich darum kümmern.');
+    });
 
 
 ///// slider
