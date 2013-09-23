c87e9322a480d5cae75f20a8fc48ed78ca9881a5
ruthverena75 <ruth.geisenfelder@gmail.com>
2013-09-23 14:42:36 +0200

    [AW-1054] adding a permalink, tinyURL and clipboard function to dialogues

---
+++ sites/all/themes/custom/parliamentwatch/js/shortlink.js
@@ -0,0 +1,9 @@
+jQuery(document).ready(function(){
+    jQuery('.shorten input').attr('checked', false); 
+    jQuery('.shorten').click(function(){
+        jQuery(this).parent().find('.pw-question-link .text-field').toggle();
+        jQuery(this).parent().find('.pw-question-link .text-field').toggleClass('permalink');
+        jQuery(this).parent().find('input').attr('checked', !jQuery(this).parent().find('input').attr('checked'));
+        return false;
+    });
+});
