ed56f3db0c8461cdde815539848e8da6d9b6609e
donSchoe <afri.alex@gmail.com>
2013-10-07 15:19:03 +0200

    Updated parliament migrate plugin and testing script.

--- sites/all/libraries/migrate-tests/parliamentTerm.php
+++ sites/all/libraries/migrate-tests/parliamentTerm.php
@@ -1,5 +1,10 @@
 <?php
 
+  //////////////////////////////////////////////////////////////////////////////
+  /////////////// Run this script from DRUSH COMMAND LINE !!! //////////////////
+  // drush [options] scr sites/all/libraries/migrate-tests/parliamentTerm.php //
+  //////////////////////////////////////////////////////////////////////////////
+
   // Starting timers ///////////////////////////////////////////////////////////
   $time_start = microtime(true);
   $rustart = getrusage();
@@ -13,79 +18,117 @@
   $xmlExpression = "//parliaments/parliament/name";
   
   // database SELECT *1 from *2 ////////////////////////////////////////////////
-  $dbSelect = 'project'; //*1
+  $dbSelect = 'parliament'; //*1
   $dbFrom = 'legacy_projects'; //*2
   
   // UTF-8 needed to display non-standard characters ///////////////////////////
   header('Content-Type: text/html; charset=utf-8');
   
   // Starting script ///////////////////////////////////////////////////////////
-  echo '<h3>Checking parliament term data integrity.</h3>';
+  watchdog("PW_TEST", t("Checking parliament term data integrity."), $variables = NULL, WATCHDOG_DEBUG, $link = NULL);
   
   // Establishing database connection //////////////////////////////////////////
   // DO NOT enter database details of production sites, rather use quickstart,..
-  if (!$link = mysql_connect('localhost', 'root', 'quickstart')) {
-    echo 'Could not connect to mySQL database!<br /><br />';
+  if (!$dblink = mysql_connect('localhost', 'root', 'quickstart')) {
+    watchdog("PW_TEST", t("Could not connect to mySQL database!"), $variables = NULL, WATCHDOG_ERROR, $link = NULL);
     exit;
   }
   // Migration database name ///////////////////////////////////////////////////
-  if (!mysql_select_db('parlamentwatch', $link)) {
-    echo 'Could not select "parlamentwatch" database.<br /><br />';
+  if (!mysql_select_db('parlamentwatch', $dblink)) {
+    watchdog("PW_TEST", t('Could not select "parlamentwatch" database!'), $variables = NULL, WATCHDOG_ERROR, $link = NULL);
     exit;
   }
   // DB query for Germany (AW) /////////////////////////////////////////////////
   $sql = 'SELECT ' . $dbSelect . ' FROM ' . $dbFrom . ' WHERE project_cmd '
        . ' NOT IN (233, 486, 974, 1036, 1010, 1475) AND project_cmd '
-       . ' BETWEEN 0 AND 2000 GROUP BY project_cmd ORDER BY project ASC';
-  $result = mysql_query($sql, $link);
+       . ' BETWEEN 0 AND 2000 GROUP BY ' . $dbSelect . ' ORDER BY ' . $dbSelect . ' ASC';
+  $result = mysql_query($sql, $dblink);
   if (!$result) {
-    echo "DB Error, could not query the database<br />";
-    echo 'MySQL Error: ' . mysql_error() . '<br /><br />';
+    watchdog("PW_TEST", t('Could not query the database!'), $variables = NULL, WATCHDOG_ERROR, $link = NULL);
+    watchdog("PW_TEST", t('MySQL Error: ' . mysql_error()), $variables = NULL, WATCHDOG_ERROR, $link = NULL);
     exit;
   }
   
   // Counting results in mirgration database ///////////////////////////////////
-  echo '<p>Looking in DB for "' . $dbSelect . '" in "' . $dbFrom . '":<br />';
+  watchdog("PW_TEST", t('Looking in DB for "' . $dbSelect . '" in "' . $dbFrom . '".'), $variables = NULL, WATCHDOG_INFO, $link = NULL);
   $i = 0;
   while ($row = mysql_fetch_assoc($result)) $i++;
-  echo 'Found <strong>' . $i . '</strong> parliament terms in DB.</p>';
+  watchdog("PW_TEST", t('Found ' . $i . ' parliament terms in DB.'), $variables = NULL, WATCHDOG_NOTICE, $link = NULL);
   
   // Counting results in XML-API ///////////////////////////////////////////////
-  echo '<p>Looking in XML for "' . $xmlExpression . '" in "<a href="' . $xmlPath
-     . '">' . $xmlPath . '</a>":<br />';
+  watchdog("PW_TEST", t('Looking in XML for "' . $xmlExpression . '" in "' . $xmlPath . '".'), $variables = NULL, WATCHDOG_INFO, $link = NULL);
   $file = file_get_contents($xmlPath);
   $xml = simplexml_load_string($file);
   $xpath = $xml->xpath($xmlExpression);
   $j = 0;
   while(list(, $node) = each($xpath)) $j++;
-  echo 'Found <strong>' . $j . '</strong> parliament terms in XML-API.</p>'
+  watchdog("PW_TEST", t('Found ' . $j . ' parliament terms in XML-API.'), $variables = NULL, WATCHDOG_NOTICE, $link = NULL);
+
+  // Counting results in Taxonomy ///////////////////////////////////////////////
+  watchdog("PW_TEST", t('Looking in taxonomy for "parliaments".'), $variables = NULL, WATCHDOG_INFO, $link = NULL);
+  $vid = taxonomy_vocabulary_machine_name_load("parliaments")->vid;
+  $terms = taxonomy_get_tree($vid);
+  $numTerms = count($terms);
+  watchdog("PW_TEST", t('Found ' . $numTerms . ' parliament terms in taxonomy.'), $variables = NULL, WATCHDOG_NOTICE, $link = NULL);
   
+  // Checking for missing entries in Taxonomy //////////////////////////////////
+  watchdog("PW_TEST", t('Checking missing entries in taxonomy...'), $variables = NULL, WATCHDOG_INFO, $link = NULL);
+  $result = mysql_query($sql, $dblink);
+  $countMissingTax = 0;
+  while ($row = mysql_fetch_assoc($result)) {
+    $isMatch = false;
+    foreach ($terms as $term) {
+      $name = $term->name;
+      if (strcmp(utf8_encode($row[$dbSelect]), $name) == 0) {
+        $isMatch = true;
+      }
+    }
+    if (!$isMatch) {
+      watchdog("PW_TEST", t('No match for: ' . utf8_encode($row[$dbSelect])), $variables = NULL, WATCHDOG_WARNING, $link = NULL);
+      $countMissingTax++;
+    }
+  }
+  $tmpString = '0';
+  $status = WATCHDOG_INFO;
+  if ($countMissingTax > 0) {
+    $status = WATCHDOG_WARNING;
+    $tmpString = $countMissingTax;
+  } else {
+    $status = WATCHDOG_NOTICE;
+    $tmpString = 'No';
+  }
+  watchdog("PW_TEST", t($tmpString . ' missing entries in taxonomy.'), $variables = NULL, $status, $link = NULL);
+
   // Checking for missing entries in XML ///////////////////////////////////////
-     . '<p>Checking missing entries in XML...<br />';
-  $result = mysql_query($sql, $link);
+  watchdog("PW_TEST", t('Checking missing entries in XML...'), $variables = NULL, WATCHDOG_INFO, $link = NULL);
+  $result = mysql_query($sql, $dblink);
   $countMissingXML = 0;
   while ($row = mysql_fetch_assoc($result)) {
     $isMatch = false;
     $xpath = $xml->xpath($xmlExpression);
     while(list(, $node) = each($xpath)) {
-      if (strcmp(utf8_encode($row['project']), $node) == 0) {
+      if (strcmp(utf8_encode($row[$dbSelect]), $node) == 0) {
         $isMatch = true;
       }
     }
     if (!$isMatch) {
-      echo '<strong>No match</strong> for: ' . utf8_encode($row['project'])
-         . '<br />';
+      watchdog("PW_TEST", t('No match for: ' . utf8_encode($row[$dbSelect])), $variables = NULL, WATCHDOG_WARNING, $link = NULL);
       $countMissingXML++;
     }
   }
   $tmpString = '0';
+  $status = WATCHDOG_INFO;
   if ($countMissingXML > 0) {
-    $tmpString = '<strong>' . $countMissingXML . '</strong>';
-  } else $tmpString = 'No';
-  echo $tmpString . ' missing entries in XML.</p>'
+    $status = WATCHDOG_WARNING;
+    $tmpString = $countMissingXML;
+  } else {
+    $status = WATCHDOG_NOTICE;
+    $tmpString = 'No';
+  }
+  watchdog("PW_TEST", t($tmpString . ' missing entries in XML.'), $variables = NULL, $status, $link = NULL);
   
   // Checking for duplicates in XML ////////////////////////////////////////////
-     . '<p>Checking for duplicates in XML...<br />';
+  watchdog("PW_TEST", t('Checking for duplicates in XML...'), $variables = NULL, WATCHDOG_INFO, $link = NULL);
   $outterXpath = $xml->xpath($xmlExpression);
   $s = 0;
   $countDuplicateXML = 0;
@@ -96,70 +139,86 @@
     while (list(, $innerNode) = each($innerXpath)) {
       $t++;
       if (strcmp($outterNode, $innerNode) == 0 && $s != $t) {
-        echo '<strong>Possible duplicate entry</strong> for: ' . $innerNode
-           . ' and ' . $outterNode . '<br />';
+        watchdog("PW_TEST", t('Possible duplicate entry for: ' . $innerNode . ' and ' . $outterNode), $variables = NULL, WATCHDOG_WARNING, $link = NULL);
         $countDuplicateXML++;
       }
     }
   }
   $countDuplicateXML /= 2;
+  $tmpString = '0';
+  $status = WATCHDOG_INFO;
   if ($countDuplicateXML > 0) {
-    $tmpString = '<strong>' . $countDuplicateXML . '</strong>';
-  } else $tmpString = 'No';
-  echo $tmpString . ' duplicate entries in XML.</p>'
+    $status = WATCHDOG_WARNING;
+    $tmpString = $countDuplicateXML;
+  } else {
+    $status = WATCHDOG_NOTICE;
+    $tmpString = 'No';
+  }
+  watchdog("PW_TEST", t($tmpString . ' duplicate entries in XML.'), $variables = NULL, $status, $link = NULL);
   
   // Checking for duplicates in DB /////////////////////////////////////////////
-     . '<p>Checking for duplicates in DB...<br />';
-  $outterResult = mysql_query($sql, $link);
+  watchdog("PW_TEST", t('Checking for duplicates in DB...'), $variables = NULL, WATCHDOG_INFO, $link = NULL);
+  watchdog("PW_TEST", t('Hint: This test result can be ignored as DB statement GROUPS BY "' . $dbSelect . '" and will find no results. Please, do a manual check on this one if desired.'), $variables = NULL, WATCHDOG_DEBUG, $link = NULL);
+  $outterResult = mysql_query($sql, $dblink);
   $s = 0;
   $countDuplicateDB = 0;
   while ($outterRow = mysql_fetch_assoc($outterResult)) {
     $s++;
-    $innerResult = mysql_query($sql, $link);
+    $innerResult = mysql_query($sql, $dblink);
     $t = 0;
     while ($innerRow = mysql_fetch_assoc($innerResult)) {
       $t++;
-      if (strcmp(utf8_encode($outterRow['project']),
-                 utf8_encode($innerRow['project'])) == 0 && $s != $t) {
-        echo '<strong>Possible duplicate entry</strong> for: ' 
-           . utf8_encode($outterRow['project']) . ' and ' 
-           . utf8_encode($innerRow['project']) . '<br />';
+      if (strcmp(utf8_encode($outterRow[$dbSelect]),
+                 utf8_encode($innerRow[$dbSelect])) == 0 && $s != $t) {
+        watchdog("PW_TEST", t('Possible duplicate entry</strong> for: ' . utf8_encode($outterRow[$dbSelect]) . ' and ' . utf8_encode($innerRow[$dbSelect])), $variables = NULL, WATCHDOG_WARNING, $link = NULL);
         $countDuplicateDB++;
       }
     }
   }
   $countDuplicateDB /= 2;
+  $tmpString = '0';
+  $status = WATCHDOG_INFO;
   if ($countDuplicateDB > 0) {
-    $tmpString = '<strong>' . $countDuplicateDB . '</strong>';
-  } else $tmpString = 'No';
-  echo $tmpString . ' duplicate entries in DB.</p>'
+    $status = WATCHDOG_WARNING;
+    $tmpString = $countDuplicateDB;
+  } else {
+    $status = WATCHDOG_NOTICE;
+    $tmpString = 'No';
+  }
+  watchdog("PW_TEST", t($tmpString . ' duplicate entries in DB.'), $variables = NULL, $status, $link = NULL);
   
   // Checking for missing entries in DB ////////////////////////////////////////
-     . '<p>Checking missing entries in DB...<br />';
+  watchdog("PW_TEST", t('Checking missing entries in DB...'), $variables = NULL, WATCHDOG_INFO, $link = NULL);
   $xpath = $xml->xpath($xmlExpression);
   $countMissingDB = 0;
   while(list(, $node) = each($xpath)) {
     $isMatch = false;
-    $result = mysql_query($sql, $link);
+    $result = mysql_query($sql, $dblink);
     while ($row = mysql_fetch_assoc($result)) {
-      if (strcmp($node, utf8_encode($row['project'])) == 0) {
+      if (strcmp($node, utf8_encode($row[$dbSelect])) == 0) {
         $isMatch = true;
       }
     }
     if (!$isMatch) {
-      echo '<strong>No match</strong> for: ' . $node . '<br />';
+      watchdog("PW_TEST", t('No match for: ' . $node), $variables = NULL, WATCHDOG_WARNING, $link = NULL);
       $countMissingDB++;
     }
   }
+  $tmpString = '0';
+  $status = WATCHDOG_INFO;
   if ($countMissingDB > 0) {
-    $tmpString = '<strong>' . $countMissingDB . '</strong>';
-  } else $tmpString = 'No';
-  echo $tmpString . ' missing entries in DB.</p>'
+    $status = WATCHDOG_WARNING;
+    $tmpString = $countMissingDB;
+  } else {
+    $status = WATCHDOG_NOTICE;
+    $tmpString = 'No';
+  }
+  watchdog("PW_TEST", t($tmpString . ' missing entries in DB.'), $variables = NULL, $status, $link = NULL);
   
   // Script finished ///////////////////////////////////////////////////////////
-     . '<p>After successful parliament term validation, you may proceed to the'
-     . ' <a href="politicianProfiles.php">politician profiles</a> validation.'
-     . '</p>';
+  //   . '<p>After successful parliament term validation, you may proceed to the'
+  //   . ' <a href="politicianProfiles.php">politician profiles</a> validation.'
+  //   . '</p>';
      
   // Getting time //////////////////////////////////////////////////////////////
   function rutime($ru, $rus, $index) {
@@ -171,10 +230,10 @@
   $ru = getrusage();
   $time_end = microtime(true);
   $execution_time = (int) (($time_end - $time_start) * 1000);
-  echo "<p><em>This script used " . rutime($ru, $rustart, "utime")
+    watchdog("PW_TEST", t("This script used " . rutime($ru, $rustart, "utime")
      . "ms for its computations and spent " . rutime($ru, $rustart, "stime")
      . "ms in system calls. Total execution time " . $execution_time
-     . "ms.</em></p>";
+     . "ms."), $variables = NULL, WATCHDOG_DEBUG, $link = NULL);
      
   //////////////////////////////////////////////////////////////////////////////
 
