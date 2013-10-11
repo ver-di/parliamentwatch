ae95589cba6869483180823e625dc2f12280c1c1
ruthverena75 <ruth.geisenfelder@gmail.com>
2013-10-11 13:42:35 +0200

    adding textselect funcionality to shortlink

--- sites/all/themes/custom/parliamentwatch/js/shortlink.js
+++ sites/all/themes/custom/parliamentwatch/js/shortlink.js
@@ -1,4 +1,3 @@
-(function(a,b,c){"use strict",a.fn.selectText=function(){var a=this[0],d=b.body.createTextRange,e=c.getSelection(),f=b.createRange(),g;b.body.createTextRange?(d.moveToElementText(a),d.select()):c.getSelection&&(e.setBaseAndExtent?e.setBaseAndExtent(g,0,g,1):(f.selectNodeContents(a),e.removeAllRanges(),e.addRange(f)))}})(jQuery,document,window)
 jQuery(document).ready(function(){
     jQuery('.shorten input').attr('checked', false); 
     jQuery('.shorten').click(function(){
@@ -8,7 +7,6 @@ jQuery(document).ready(function(){
         return false;
     });
     jQuery('.permalink-wrapper .text-field').click(function(){
-        //alert('foo');
-        jQuery(this).selectText();
+        jQuery(this).selectText(); //working with https://github.com/emilkje/jquery.selectText
     });
 });
