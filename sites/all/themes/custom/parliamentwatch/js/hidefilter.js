40fd87a4be08f505e33fcc9795a82ed290650204
Oliver Pal <oliver.pal@intersignum.com>
2013-11-07 10:23:05 +0100

    add profile features to stage branch

---
+++ sites/all/themes/custom/parliamentwatch/js/hidefilter.js
@@ -0,0 +1,14 @@
+/**
+ * Created by oliverpal on 06.11.13.
+ */
+jQuery(document).ready(function(){
+    var foundin = jQuery('div');
+
+    console.log(foundin);
+    // /console.log(jQuery('.bubble').html());
+    if(true){
+    jQuery('.ds-right-2').hide("fast");
+    jQuery('#pw-block-questions-and-answers').hide("fast");
+    jQuery('#form-view-mode-switcher').hide("fast");
+    }
+});
