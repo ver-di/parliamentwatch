819f060a187f2abb36c8ec40c7062e0fa325c4eb
ruthverena75 <ruth.geisenfelder@gmail.com>
2014-03-17 20:09:36 +0100

    [AW-1489] adding kandidatenwatch feature

---
+++ sites/all/modules/custom/pw_kandidatencheck/pw_kandidatencheck.js
@@ -0,0 +1,4 @@
+jQuery(document).ready(function() {
+    jQuery(".view-pw-kandidatencheck .views_slideshow_controls_text_next a").text(Drupal.t('next thesis'));
+    jQuery(".view-pw-kandidatencheck .views_slideshow_controls_text_previous a").text(Drupal.t('previous thesis'));
+});
