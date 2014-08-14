7a4cb72f802da014ae8d0dc8a1b34361324002d7
ruthverena75 <geisenfelder@abgeordnetenwatch.de>
2014-08-14 13:24:22 +0200

    AW-1944 #comment add login template with förderer hint

--- sites/abgeordnetenwatch.de/themes/abgeordnetenwatch/template.php
+++ sites/abgeordnetenwatch.de/themes/abgeordnetenwatch/template.php
@@ -1,2 +1,13 @@
 <?php
-// AW-1500
+function abgeordnetenwatch_theme(&$existing, $type, $theme, $path) {
+  $hooks['user_login'] = array(
+    'template' => 'templates/user-login',
+    'render element' => 'form',
+    // other theme registration code...
+  );
+  return $hooks;
+}
+function mytheme_preprocess_user_login(&$variables) {
+  $variables['intro_text'] = '';
+  $variables['rendered'] = drupal_render_children($variables['form']);
+}
---
+++ sites/abgeordnetenwatch.de/themes/abgeordnetenwatch/templates/user-login.tpl.php
@@ -0,0 +1,22 @@
+<p><?php print render($intro_text); ?></p>
+
+<div class="grid-4 alpha">
+    <h3>Förderer</h3>
+    <p>
+    	Sie sind <strong>Förderer von abgeordnetenwatch.de</strong>? Nutzen Sie bitte den Login unter folgendem Link:
+    </p>
+    <p class="rtecenter">
+    	<a class="button big" href="https://secured.abgeordnetenwatch.de/login_bereich-846-0.html">Förderer-Login</a>
+    </p>
+    <p>Noch kein Fördermitglied? Werden Sie jetzt Förderer von abgeordnetenwatch.de und unterstützen Sie uns durch eine regelmäßige Spende dabei, mehr Transparenz und Bürgernähe in die Politik zu bringen. Nutzen Sie die exklusiven Vorteile einer Fördermitgliedschaft und behalten Sie Ihre Abgeordneten genau im Auge.</p>
+    <p><a href="/ueber-uns/spendenformular">Ich möchte jetzt Förderer von abgeordnetenwatch.de werden</a></p>
+</div>
+
+<div class="grid-4 omega">
+    <h3>Kandidierende</h3>
+    <p>Sie <strong>kandidieren</strong> zu den Wahlen und möchten Ihr Profil auf abgeordnetenwatch.de bearbeiten?<br/>
+    Loggen Sie sich bitte hier ein:</p>
+    <div class="yourtheme-user-login-form-wrapper">
+      <?php print drupal_render_children($form) ?>
+    </div>
+</div>
