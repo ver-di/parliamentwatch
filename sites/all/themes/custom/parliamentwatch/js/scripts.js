c3f4b35515bc00b600ac0a9a212f2cf3ea772c6a
Boris Hekele <hekele@parliamentwatch.org>
2014-08-18 15:30:19 +0200

    disable slick JS code

--- sites/all/themes/custom/parliamentwatch/js/scripts.js
+++ sites/all/themes/custom/parliamentwatch/js/scripts.js
@@ -9,19 +9,19 @@ jQuery(document).ready(function() {
 // slick kandidatencheck KC
 
     // init slick on KC
-    $('.view-id-pw_kandidatencheck .view-content').slick({
+   /* $('.view-id-pw_kandidatencheck .view-content').slick({
         autoplay: true,
         autoplaySpeed: 12000,
         nextArrow: '<button type="button" class="slick-next">'+Drupal.t('Zur nächsten These')+'<span></span></button>',
         prevArrow: '<button type="button" class="slick-prev"><span></span>'+Drupal.t('Zur vorherigen These')+'</button>'
-    });
+    });*/
     // unslick KC    
-    $('.view-id-pw_kandidatencheck .unslick').on('click', function() { // execute on slider change by sliding or cklicking
+/*    $('.view-id-pw_kandidatencheck .unslick').on('click', function() { // execute on slider change by sliding or cklicking
         $('.view-id-pw_kandidatencheck .view-content').unslick(          
             goToByScroll("pw-block-user-kc"),
             $('.view-id-pw_kandidatencheck .unslick').hide()        
         );
-    });
+    });*/
 
 
 // expand active user revision block in user revision switch
