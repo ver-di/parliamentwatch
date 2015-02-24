8faa741724772d3d4d329337f8eab48a4571671f
Stefan Ruh <ruh@abgeordnetenwatch.de>
2015-02-24 14:56:30 +0100

    Themes the image for the petition teaser template.

--- sites/all/themes/custom/parliamentwatch/templates/node--pw-petition--teaser.tpl.php
+++ sites/all/themes/custom/parliamentwatch/templates/node--pw-petition--teaser.tpl.php
@@ -13,12 +13,16 @@ switch ($field_petition_partner['und'][0]['value']) {
     $signing_url = $field_petition_external_url['und'][0]['url'];
     break;
 }
+$themed_image = theme_image_style(array(
+  'style_name' => 'pw_landscape_l__normal', //Configure style here!
+  'path' => $field_teaser_image['und'][0]['uri']
+));
 ?>
 <hr>
 <h2>Titel: <? echo "<a href=\"".$node_url."\">".$title."</a>"; ?></h2>
 <div>
   <ul>
-    <li>Bild:  <? echo file_create_url($field_teaser_image['und'][0]['uri']); ?></li>
+    <li>Bild:  <? echo $themed_image; ?></li>
     <li>Copyright: <? echo $field_teaser_image['und'][0]['field_image_copyright']['und'][0]['value'] ?></li>
     <li>Shares: <? echo (isset($field_share_sum['und'][0]['value']))?$field_share_sum['und'][0]['value']:"0"; ?></li>
     <li>Benötigte Unterschriften: <? echo $field_petition_required['und'][0]['value']; ?></li>
