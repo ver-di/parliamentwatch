d7c4332656d144d069b21a4a628689588c62976f
Stefan Ruh <ruh@abgeordnetenwatch.de>
2015-02-24 12:34:57 +0100

    Add dynamic behaviour (logo and signing link) based on petition partner organisation.

--- sites/all/themes/custom/parliamentwatch/templates/node--pw-petition--teaser.tpl.php
+++ sites/all/themes/custom/parliamentwatch/templates/node--pw-petition--teaser.tpl.php
@@ -1,3 +1,19 @@
+<?php
+switch ($field_petition_partner['und'][0]['value']) {
+  case "":
+    $partner_html = "&nsbp;";
+    $signing_url = $node_url;
+    break;
+  case "change.org":
+    $partner_html = "<img src='' alt='Logo von Change.org'/>";
+    $signing_url = $field_petition_external_url['und'][0]['url'];
+    break;
+  case "openpetition":
+    $partner_html = "<img src='' alt='Logo von OpenPetition'/>";
+    $signing_url = $field_petition_external_url['und'][0]['url'];
+    break;
+}
+?>
 <hr>
 <h2>Titel: <? echo "<a href=\"".$node_url."\">".$title."</a>"; ?></h2>
 <div>
@@ -8,6 +24,7 @@
     <li>Benötigte Unterschriften: <? echo $field_petition_required['und'][0]['value']; ?></li>
     <li>Erhaltene Unterschriften: <? echo $field_petition_signings['und'][0]['value']; ?></li>
     <li>Unterschriftenfortschritt: <? echo $field_petition_progress['und'][0]['value']; ?></li>
-    <li>Partnerorganisation: <? echo $field_petition_partner['und'][0]['value']; ?></li>
+    <li>Partnerorganisation: <? echo $partner_html; ?></li>
+    <li><a href="<? echo $signing_url; ?>">Hier unterschreiben</a></li>
   </ul>
 </div>
