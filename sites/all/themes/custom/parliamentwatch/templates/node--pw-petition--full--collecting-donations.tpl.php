5af9a1e7305919aa3c0e66c138b3f34399f7013d
Stefan Ruh <ruh@abgeordnetenwatch.de>
2015-03-17 10:51:34 +0100

    AW-2354: Adds template for full donation page and introduces minor fixes for the teaser templates.

--- sites/all/themes/custom/parliamentwatch/templates/node--pw-petition--full--collecting-donations.tpl.php
+++ sites/all/themes/custom/parliamentwatch/templates/node--pw-petition--full--collecting-donations.tpl.php
@@ -1,24 +1,86 @@
-<hr>
-Full view for collecting donations.
-<h2>Titel: <? echo "<a href=\"".$node_url."\">".$title."</a>"; ?></h2>
-<div>
-  <ul>
-    <li>Bild:  <? echo file_create_url($field_teaser_image[0]['uri']); ?></li>
-    <li>Copyright: <? echo $field_teaser_image[0]['field_image_copyright']['und'][0]['value'] ?></li>
-    <li>Shares: <? echo (isset($field_share_sum['und'][0]['value']))?$field_share_sum['und'][0]['value']:"0"; ?></li>
-    <li>Benötigte Spendensumme: <? echo $field_donation_required['und'][0]['value']; ?></li>
-    <li>Erhaltene Spendensumme: <? echo $field_donation_amount['und'][0]['value']; ?></li>
-    <li>Spendenfortschritt: <? echo $field_donation_progress['und'][0]['value']; ?></li>
-    <li>Partnerorganisation: <? echo $field_petition_partner['und'][0]['value']; ?></li>
-  </ul>
-</div>
 <?
+//todo: field_get_items benutzen
 $block = block_load('webform','client-block-10508');
 $a = _block_render_blocks(array($block));
 $a['webform_client-block-10508']->subject = "";
-print  drupal_render(_block_get_renderable_array($a));
+$signing_form = drupal_render(_block_get_renderable_array($a));
 
-// Render content only
-//$block = module_invoke('webform', 'block_view', 'client-block-10508');
-//print render($block['content']);
+switch ($field_petition_partner['und'][0]['value']) {
+  case "":
+    $partner_html = "";
+    $signing_url = $node_url;
+    $sharethis = "<span st_url=\"https://www.abgeordnetenwatch.de$node_url\" st_title=\"$title\" st_summary=\"Petition $title auf abgeordnetenwatch.de\" class=\"st_sharethis_hcount\" displayText=\"sharethis\"></span>";
+    break;
+  case "change.org":
+    $partner_html = '<img src="/sites/all/themes/custom/parliamentwatch/images/logo-change.png" width="119" height="23" alt="Change.org">';
+    //$signing_url = "https://secured.abgeordnetenwatch.de/tools/newsletter.php?width=800&height=450&iframe=true&continue=".urlencode($field_petition_external_url['und'][0]['url']);
+    //$node_url = $signing_url;
+    break;
+  case "openpetition":
+    $partner_html = '<img src="/sites/all/themes/custom/parliamentwatch/images/logo-openpetition.png" width="119" height="36" alt="OpenPetition">';
+    //$signing_url = $field_petition_external_url['und'][0]['url'];
+    //$node_url = $signing_url;
+    break;
+}
+
+$themed_image = theme_image_style(array(
+  'style_name' => 'pw_landscape_l', //Configure style here!
+  'path' => $field_teaser_image[0]['uri']
+));
+$comments = render(comment_node_page_additions($node)['comments']);
+
+if (!empty($field_teaser_image[0]['field_image_copyright']) || !empty($field_teaser_image[0]['field_image_copyright']['und'][0]['value'] )){
+  $field_image_copyright = true;
+}
 ?>
+<?php if ($sharethis): ?>
+  <div class="sharethis-wrapper">
+    <? echo $sharethis; ?>
+  </div>
+<?php endif; ?>
+<div class="push-bottom-l">
+  <? echo $field_petition_text_donation[0]['value'] ?>
+  <p class="medium"><? echo $field_petition_recipient[0]['value'] ?></p>
+
+  <?php if (!empty($field_blogpost_blogtags)): ?>
+    <div class="icon-taxonomy">
+      <?
+      $first_term = true;
+      foreach ($field_blogpost_blogtags as $key => $value){
+        if ($first_term) {
+          $first_term = false;
+        }
+        else{
+          echo ", ";
+        }
+        echo l($value['taxonomy_term']->name, 'taxonomy/term/' . $value['tid']);
+      }
+      ?>
+    </div>
+  <?php endif; ?>
+
+</div>
+<div class="push-bottom-l">
+  <h3>Für Meinungsumfrage spenden</h3>
+  <? echo $signing_form; ?>
+</div>
+<div class="managed-content push-bottom-l">
+  <h3><? echo $title; ?></h3>
+  <div class="file-image float-left push-bottom-s">
+    <div class="content">
+      <? echo $themed_image; ?>
+      <?php if ($field_image_copyright): ?>
+        <div class="copyright">
+          <? echo $field_teaser_image[0]['field_image_copyright']['und'][0]['value'] ?>
+        </div>
+      <?php endif; ?>
+    </div>
+  </div>
+  <? echo $body[0]['value']; ?>
+</div>
+<?php if ($comments): ?>
+<div id="comments" class="comment-wrapper">
+  <h3>Ich habe die Petition unterschrieben, weil...</h3>
+  <? echo $comments; ?>
+</div>
+<?php endif; ?>
