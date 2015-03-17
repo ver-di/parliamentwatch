1f90fc7c51032978a7687aed5b64918703817573
Stefan Ruh <ruh@abgeordnetenwatch.de>
2015-03-17 16:51:59 +0100

    AW-2354: Changes full donation page for petitions according to the main template.

--- sites/all/themes/custom/parliamentwatch/templates/node--pw-petition--full--collecting-donations.tpl.php
+++ sites/all/themes/custom/parliamentwatch/templates/node--pw-petition--full--collecting-donations.tpl.php
@@ -39,11 +39,11 @@ if (!empty($field_teaser_image[0]['field_image_copyright']) || !empty($field_tea
   </div>
 <?php endif; ?>
 <div class="push-bottom-l">
-  <? echo $field_petition_text_donation[0]['value'] ?>
-  <p class="medium"><? echo $field_petition_recipient[0]['value'] ?></p>
+
+  <div class="medium"><? echo $field_petition_recipient[0]['value'] ?></div>
 
   <?php if (!empty($field_blogpost_blogtags)): ?>
-    <div class="icon-taxonomy">
+    <div class="icon-taxonomy push-bottom-m">
       <?
       $first_term = true;
       foreach ($field_blogpost_blogtags as $key => $value){
@@ -58,7 +58,10 @@ if (!empty($field_teaser_image[0]['field_image_copyright']) || !empty($field_tea
       ?>
     </div>
   <?php endif; ?>
-
+  <div class="pw-petition-progress-wrapper float-left"><div class="pw-petition-progress" style="width: <? echo $field_donation_progress[0]['value'];?>%;"></div></div>
+  <div class="medium clear"><h4 class="label-inline">Kosten Meinungsumfrage:&nbsp;</h4><strong><? echo number_format($field_donation_required[0]['value'],0,',','.'); ?>&nbsp;&euro;</strong></div>
+  <div class="light small push-bottom-m"><?echo number_format($field_donation_amount[0]['value'],0,',','.')?>&nbsp;&euro; wurden bereits gespendet</div>
+  <? echo $field_petition_text_donation[0]['value'] ?>
 </div>
 <div class="push-bottom-l">
   <h3>Für Meinungsumfrage spenden</h3>
