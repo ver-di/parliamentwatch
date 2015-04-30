0b025270faf5cca6d0319030c8f61a325ea86baa
ruthverena75z <geisenfelder@abgeordnetenwatch.de>
2015-04-30 12:58:32 +0200

    style asking parliament

--- sites/all/themes/custom/parliamentwatch/templates/node--pw-petition--full--asking-parliament.tpl.php
+++ sites/all/themes/custom/parliamentwatch/templates/node--pw-petition--full--asking-parliament.tpl.php
@@ -1,58 +1,62 @@
 <?php if ($sharethis): ?>
-    <div class="sharethis-wrapper">
-        <? print $sharethis; ?>
-    </div>
+  <div class="sharethis-wrapper">
+    <? print $sharethis; ?>
+  </div>
 <?php endif; ?>
+
+  <div class="floatbox floatbox-right">
+      <i class="icon-signing aw-icon-1x aw-icon-circle aw-icon-circle-brand float-left push-right-s push-bottom-xs"><span class="element-invisible">Unterschriften werden gesammelt</span></i>
+      <p class="pushfloat-0">Die Petition hat <?echo number_format($field_petition_signings[0]['value'],0,',','.')?> von <? echo number_format($field_petition_required[0]['value'],0,',','.'); ?> benötigten Unterschriften erreicht.</p>
+      <i class="icon-microphone aw-icon-1x aw-icon-circle aw-icon-circle-brand float-left push-right-s push-bottom-xs"><span class="element-invisible">Petition in der Meinungsumfrage</span></i>
+      <p class="pushfloat-0">Laut repräsentativer Meinungsumfrage genießt das Anliegen eine Mehrheit in der Bevölkerung.</p>
+      <i class="icon-politician aw-icon-1x aw-icon-circle aw-icon-circle-disabled float-left push-right-s push-bottom-xs"><span class="element-invisible">Petition im Parlament</span></i>
+      <p class="pushfloat-0">Die Petition wird aktuell im Parlament abgefragt.</p>
+  </div>
 <strong><? print check_markup($body[0]['summary']); ?></strong>
-<p class="medium">Adressat: <? print $field_petition_recipient[0]['value'] ?></p>
+<p class="medium">
+  Adressat: <? print $field_petition_recipient[0]['value'] ?>
+</p>
 
 <?php if (!empty($field_blogpost_blogtags)): ?>
-    <p class="icon-taxonomy push-bottom-m">
-        <?
-        $first_term = true;
-        foreach ($field_blogpost_blogtags as $key => $value){
-            if ($first_term) {
-                $first_term = false;
-            }
-            else{
-                print ", ";
-            }
-            $term = taxonomy_term_load($value['tid']);
-            print l($term->name, 'taxonomy/term/' . $value['tid']);
-        }
-        ?>
-    </p>
+  <p class="icon-taxonomy push-bottom-m">
+    <?
+    $first_term = true;
+    foreach ($field_blogpost_blogtags as $key => $value){
+      if ($first_term) {
+        $first_term = false;
+      }
+      else{
+        print ", ";
+      }
+      $term = taxonomy_term_load($value['tid']);
+      print l($term->name, 'taxonomy/term/' . $value['tid']);
+    }
+    ?>
+  </p>
 <?php endif; ?>
 
 <?php
-// render webform block for politicians if parameter "u" is in url
-$getparams = drupal_get_query_parameters();
-if (isset($getparams['u'])) {
-  $block = module_invoke('webform', 'block_view', 'client-block-57030');
-  print render($block['content']);
-}
-else {
-
-}
+  // render webform block for politicians if parameter "u" is in url
+  $getparams = drupal_get_query_parameters();
+  if (isset($getparams['u'])) {
+    $block = module_invoke('webform', 'block_view', 'client-block-57030');
+    print render($block['content']);
+  }
 ?>
 
-<h3>Aktuelle Positionen</h3>
-<p class="medium">Hier werden die aktuellsten fuenf Positionen angezeigt. Sobald X Abgeordnete Stellung bezogen haben, werden wir hier eine ausfuherliche Auswertung anzeigen.</p>
+<h3 class="clear">Aktuelle Positionen</h3>
+
+<p class="medium">Hier werden die aktuellsten fünf Positionen angezeigt. Sobald eine ausreichende Anzahl Abgeordneter Stellung bezogen haben, werden wir hier eine ausführliche Auswertung anzeigen.</p>
 <?php
 // render block of latest positions
-$block = module_invoke('views', 'block_view', 'pw_vote_search-block_1');
-print render($block['content']);
+  $block = module_invoke('views', 'block_view', 'pw_vote_search-block_1');
+  print render($block['content']);
 ?>
 
 <h3>Hintergrund</h3>
 <div class="managed_content push-bottom-l">
   <? print check_markup($field_petition_text_parliament[0]['value']); ?>
 </div>
-<div class="pw-progress-wrapper pw-progress-wrapper-l grid-5 alpha">
-  <div class="pw-progress" style="width: <? echo $field_petition_progress[0]['value'];?>%;"></div>
-</div>
-<div class="medium clear"><h4 class="label-inline">Benötigte Unterschriften:&nbsp;</h4><strong><? echo number_format($field_petition_required[0]['value'],0,',','.'); ?></strong></div>
-<div class="light small push-bottom-l"><?echo number_format($field_petition_signings[0]['value'],0,',','.')?> unterstützen die Petition</div>
 
 <?php 
 // render comments if there are any
--- sites/all/themes/custom/parliamentwatch/templates/views-view-fields--pw-vote-search.tpl.php
+++ sites/all/themes/custom/parliamentwatch/templates/views-view-fields--pw-vote-search.tpl.php
@@ -1,31 +1,38 @@
 <?php
-$image = theme('image_style', array(
-  'style_name' => 'pw_portrait_xs', //Configure style here!
-  'path' => $row->ss_vote_user_image_uri[0],
-));
+  $image = theme('image_style', array(
+    'style_name' => 'pw_portrait_xs', //Configure style here!
+    'path' => $row->ss_vote_user_image_uri[0],
+  ));
 ?>
-<h2><a href="/profile/<? echo $row->ss_vote_user_drupal_name[0];?>"><? echo $row->ss_vote_user_full_name[0]; ?> (<? echo $row->ss_vote_user_party[0]?>)</a> zu "<? echo $row->ss_vote_node_title[0]; ?>"</h2>
-<small>Position von <? echo $row->ss_vote_user_full_name[0]; ?>:
-<?php if ($row->ss_vote_user_vote_text[0] == "yes"): ?>
-    stimme zu
-<?php endif; ?>
-<?php if ($row->ss_vote_user_vote_text[0] == "no"): ?>
-    lehne ab
-<?php endif; ?>
-<?php if ($row->ss_vote_user_vote_text[0] == "abstain"): ?>
-    enthalten
-<?php endif; ?>
-<?php if ($row->ss_vote_user_vote_text[0] == "no-show"): ?>
-    nicht teilgenommen
-<?php endif; ?>
-</small>
-<br/>
-<? echo $image ?>
-<?php if (strlen($row->ss_vote_reason_full[0]) > 0): ?>
-<div>
-  <blockquote><? echo $row->ss_vote_reason_full[0]; ?></blockquote>
+<div class="pw-list-item clearfix">
+  <h4 class="push-bottom-xs">
+    <a href="/profile/<? echo $row->ss_vote_user_drupal_name[0];?>"><? echo $row->ss_vote_user_full_name[0]; ?> (<? echo $row->ss_vote_user_party[0]?>)</a> zu "<? echo $row->ss_vote_node_title[0]; ?>"
+  </h4>
+  <div class="push-bottom-s">
+    <span class="pw-voting">
+      Position von <? echo $row->ss_vote_user_full_name[0]; ?>:
+      <?php if ($row->ss_vote_user_vote_text[0] == "yes"): ?>
+        <span class="yes block vote">lehne ab</span>
+      <?php endif; ?>
+      <?php if ($row->ss_vote_user_vote_text[0] == "no"): ?>
+        <span class="no block vote">lehne ab</span>
+      <?php endif; ?>
+      <?php if ($row->ss_vote_user_vote_text[0] == "abstain"): ?>
+        <span class="abstain block vote">stimme zu</span>
+      <?php endif; ?>
+      <?php if ($row->ss_vote_user_vote_text[0] == "no-show"): ?>
+        <span class="no-show block vote">nicht teilgenommen</span>
+      <?php endif; ?>
+    </span>
+  </div>
+  <div class="float-left pw-kc-profile-picture push-bottom-s">
+    <div class="file-image">
+      <? echo $image ?>
+    </div>
+  </div>
+  <?php if (strlen($row->ss_vote_reason_full[0]) > 0): ?>
+  <div class="pw-kc-argumentation pushfloat-1">
+    <blockquote><? echo $row->ss_vote_reason_full[0]; ?></blockquote>
+  </div>
+  <?php endif; ?>
 </div>
-<?php endif; ?>
-
-<!--<blockquote><? echo $row->ss_vote_reason_summary[0]; ?></blockquote>-->
-<hr/>
