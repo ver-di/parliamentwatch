22cf87b179dedbe4205c69199757ad0872728c46
Boris Hekele <hekele@parliamentwatch.org>
2015-05-21 17:14:00 +0200

    refine petition templates

--- sites/all/themes/custom/parliamentwatch/templates/node--pw-petition--full--passed-parliament.tpl.php
+++ sites/all/themes/custom/parliamentwatch/templates/node--pw-petition--full--passed-parliament.tpl.php
@@ -3,10 +3,12 @@
 ?>
 <?php if ($sharethis): ?>
   <div class="sharethis-wrapper">
-      <? echo $sharethis; ?>
+      <? print $sharethis; ?>
   </div>
 <?php endif; ?>
-<p class="medium"><? echo $field_petition_recipient[0]['value'] ?></p>
+<p class="medium">Adressat: <? print $field_petition_recipient[0]['value'] ?></p>
+
+<? print check_markup($field_petition_text_passed[0]['summary']); ?>
 
 <?php if (!empty($field_blogpost_blogtags)): ?>
     <p class="icon-taxonomy push-bottom-m">
@@ -17,18 +19,14 @@
                 $first_term = false;
             }
             else{
-                echo ", ";
+                print ", ";
             }
             $term = taxonomy_term_load($value['tid']);
-            echo l($term->name, 'taxonomy/term/' . $value['tid']);
+            print l($term->name, 'taxonomy/term/' . $value['tid']);
         }
         ?>
     </p>
 <?php endif; ?>
-
-<div class="managed_content push-bottom-l">
-  <? print check_markup($field_petition_text_parliament[0]['value']); ?>
-</div>
 
 <?php
   $block = module_invoke('pw_vote', 'block_view', 'voting_behavior');
@@ -40,13 +38,22 @@
 <div class="push-bottom-l">SUCHERGEBNISSE</div>
 
 <h3>Hintergrund</h3>
-<div class="clearfix push-bottom-l managed-content">
-  <? echo $body[0]['value']; ?>
+<div class="managed-content clearfix push-bottom-l">
+  <div class="floatbox floatbox-right">
+    <i class="icon-signing aw-icon-1x aw-icon-circle aw-icon-circle-brand float-left push-right-s push-bottom-xs"><span class="element-invisible">Unterschriften werden gesammelt</span></i>
+    <p class="pushfloat-0">Die Petition hat <? print number_format($field_petition_signings[0]['value'],0,',','.')?> von <? print number_format($field_petition_required[0]['value'],0,',','.'); ?> benötigten Unterschriften erreicht.</p>
+    <i class="icon-microphone aw-icon-1x aw-icon-circle aw-icon-circle-brand float-left push-right-s push-bottom-xs"><span class="element-invisible">Petition in der Meinungsumfrage</span></i>
+    <p class="pushfloat-0">Laut repräsentativer Meinungsumfrage genießt das Anliegen eine Mehrheit in der Bevölkerung.</p>
+    <i class="icon-politician aw-icon-1x aw-icon-circle aw-icon-circle-disabled float-left push-right-s push-bottom-xs"><span class="element-invisible">Petition im Parlament</span></i>
+    <p class="pushfloat-0">Die Petition wird aktuell im Parlament abgefragt.</p>
+  </div>
+  <? print check_markup($field_petition_text_passed[0]['value']); ?>
 </div>
-<h3>Inhalt der Petition</h3>
-<div class="managed_content push-bottom-l">
-  <? print check_markup($field_petition_content[0]['value']); ?>
-</div>
+
+<h3>Inhalt der Bürger-Petition (gestartet von <? print $field_petition_starter[0]['value']; ?>)</h3>
+<p class="managed-content">
+  Lesen Sie die Original-Petition auf <? print l($field_petition_external_url[0]['url'], $field_petition_external_url[0]['url']); ?>
+</p>
 
 <?php 
 // render comments if there are any
@@ -54,6 +61,6 @@ if ($comments):
 ?>
   <div id="comments" class="comment-wrapper">
     <h3>Ich habe die Petition unterschrieben, weil...</h3>
-    <? echo $comments; ?>
+    <? print $comments; ?>
   </div>
 <?php endif; ?>
