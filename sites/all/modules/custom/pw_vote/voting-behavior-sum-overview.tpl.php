23055963293b705b7de7ac6631b4e56d634b8f3d
Boris Hekele <hekele@parliamentwatch.org>
2015-05-28 11:25:17 +0200

    AW-2544 add theme function to display summarized votes for parties

--- sites/all/modules/custom/pw_vote/voting-behavior-sum-overview.tpl.php
+++ sites/all/modules/custom/pw_vote/voting-behavior-sum-overview.tpl.php
@@ -3,11 +3,3 @@
     <?php print $sum; ?> <?php print $voting_behavior; ?>
   </a>
 </li>
-
-<!--
-<li class="small light push-right-m">
-  <a href="<?php print $path_search; ?>" title="Details anzeigen">
-    <?php print $sum; ?> <?php print $voting_behavior; ?>
-  </a>
-</li>
--->
---
+++ sites/all/modules/custom/pw_vote/voting-behavior-sum-party.tpl.php
@@ -0,0 +1,5 @@
+<li class="small light push-right-m">
+  <a href="<?php print $path_search; ?>" title="Details anzeigen">
+    <?php print $sum; ?> <?php print $voting_behavior; ?>
+  </a>
+</li>
