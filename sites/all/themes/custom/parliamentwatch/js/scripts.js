68c2005d516378ce796954e4425143f8e4c27e0e
Boris Hekele <hekele@parliamentwatch.org>
2015-06-15 14:07:56 +0200

    Revert "fix expander missing file"
    This reverts commit 06f7c881e1dd16b10a75c658fe4809eba69d7a26.

--- sites/all/themes/custom/parliamentwatch/js/scripts.js
+++ sites/all/themes/custom/parliamentwatch/js/scripts.js
@@ -122,10 +122,8 @@ jQuery(document).ready(function() {
           });
         }
         pw_expand();
-          $('.summary p:last').css({display: 'inline'});
         $(document).ajaxComplete(function() {
           pw_expand();
-          $('.summary p:last').css({display: 'inline'});
         });
     });
 
