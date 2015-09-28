92361e999c9c7b6030aaea1f3d56164872d19777
ruthverena75z <geisenfelder@abgeordnetenwatch.de>
2015-09-28 13:29:14 +0200

    AW-2926 style blog teaser

--- sites/all/themes/custom/parliamentwatch/templates/node--blogpost--teaser.tpl.php
+++ sites/all/themes/custom/parliamentwatch/templates/node--blogpost--teaser.tpl.php
@@ -8,19 +8,23 @@
   von <a href="/feedback?url=<?php print $nid; ?>&amp;width=600&amp;height=400" class="colorbox-node"><?php print $node->name; ?></a> in
   <?php print _pw_get_linked_terms($field_blogpost_categories); ?>
 </p>
-<div class="float-left push-bottom-s push-right-m pw-landscape-l">
-  <a href="<?php print $node_url; ?>" title="zum Blogartikel">
-    <?php print render($content['field_teaser_image']); ?>
-  </a>
-  <?php if (!empty($field_teaser_image[0]['field_image_copyright']['und'][0]['value'])): ?>
-    <div class="copyright">
-      <?php print $field_teaser_image[0]['field_image_copyright']['und'][0]['value']; ?>
-    </div>
-  <?php endif; ?>
-</div>
 <div class="clearfix push-bottom-s">
-  <?php print check_markup($body[0]['summary']); ?>
-  <a class="icon-more" href="<?php print $node_url; ?>" title="zum Blogartikel"> weiterlesen</a>
+  <?php if (!empty($field_teaser_image)): ?>
+  <div class="float-left push-right-m pw-landscape-l file file-image">
+    <a href="<?php print $node_url; ?>" title="zum Blogartikel">
+      <?php print render($content['field_teaser_image']); ?>
+    </a>
+    <?php if (!empty($field_teaser_image[0]['field_image_copyright']['und'][0]['value'])): ?>
+      <div class="copyright">
+        <?php print $field_teaser_image[0]['field_image_copyright']['und'][0]['value']; ?>
+      </div>
+    <?php endif; ?>
+  </div>
+  <?php endif; ?>
+  <div class="field-body">
+    <?php print check_markup($body[0]['summary']); ?>
+    <a class="icon-more" href="<?php print $node_url; ?>" title="zum Blogartikel"> weiterlesen</a>
+  </div>
 </div>
 <?php if (!empty($field_blogpost_blogtags)): ?>
   <div class="icon-taxonomy">
