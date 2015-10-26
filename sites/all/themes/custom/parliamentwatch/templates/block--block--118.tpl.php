9d9bcad264f62da2400e48ea6b9d01cfb406bbf8
Boris Hekele <hekele@parliamentwatch.org>
2015-10-26 11:20:26 +0100

    Template counter caching fix

--- sites/all/themes/custom/parliamentwatch/templates/block--block--118.tpl.php
+++ sites/all/themes/custom/parliamentwatch/templates/block--block--118.tpl.php
@@ -7,20 +7,40 @@
             <div id="logo-transparenz"></div>
             <div class="content-inner">
               <?php print $content ?>
-              <strong class="counter medium"><?php print file_get_contents( "/var/www/live_abgeordnetenwatch.de/httpdocs/sites/abgeordnetenwatch.de/files/membership-count.txt" ); ?></strong> Förderer unterstützen bereits unsere Arbeit. Seien auch Sie dabei!
-            </div>
+              <strong class="counter medium" id="mscount"><?php print file_get_contents( "/var/www/live_abgeordnetenwatch.de/httpdocs/sites/abgeordnetenwatch.de/files/membership-count.txt" ); ?></strong> Förderer unterstützen bereits unsere Arbeit. Seien auch Sie dabei!
+            </div>            
           </div>
           <div class="table-cell grid-4 center table-cell-middle desktop-only">
-            <a href="/node/10508" class="button push-bottom-0"><span class="icon-thanks aw-icon-2x"></span>Jetzt fördern</a>
+            <a href="/ueber-uns/spendenformular?recurring=1" class="button push-bottom-0"><span class="icon-thanks aw-icon-2x"></span>Jetzt fördern</a>
           </div>
           <div class="table-cell table-cell-bottom grid-6 omega light small text-right desktop-only">
             <img src="/sites/all/themes/custom/parliamentwatch/images/portrait-frederik-roese.png" alt="Frederik Röse" width="69" height="75" class="float-right" />
             <div class="float-right">
               Frederik Röse<br>
-              Förderbetreuung
+              Fördererbetreuung
             </div>
           </div>
         </div>
     </div>
   </div>
+  <script>
+  jQuery(document).ready(
+    function() {
+     setInterval(function() {
+      //jQuery.ajaxSetup({ cache: false });
+      jQuery.get("/sites/abgeordnetenwatch.de/files/membership-count.txt",
+        function(count){
+            var old_count = document.getElementById('mscount').innerHTML;
+            if (old_count != count){
+              jQuery("#mscount").fadeOut('slow', function(){                                
+                jQuery('#mscount').html(count);
+                jQuery("#mscount").fadeIn('slow');
+                              }
+             );
+            }
+          }
+          );
+      }, 30000);
+    });
+</script>
 </<?php print $tag; ?>>
