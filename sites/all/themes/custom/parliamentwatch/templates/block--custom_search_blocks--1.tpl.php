d055a8dfaf018722dbc2eea53ada52523e7ed30a
Boris Hekele <hekele@parliamentwatch.org>
2015-12-08 09:12:16 +0100

    AW-3014 Makes search form usable

--- sites/all/themes/custom/parliamentwatch/templates/block--custom_search_blocks--1.tpl.php
+++ sites/all/themes/custom/parliamentwatch/templates/block--custom_search_blocks--1.tpl.php
@@ -2,7 +2,7 @@
   <div id="search-wrapper-inner">
     <a id="search-trigger" class="trigger icon-search aw-icon-4x mobile-only" href="#"><span class="arrow_box"></span></a>
     <div class="content">
-      <form class="search-form" action="." method="get" id="custom-search-blocks-form-1" accept-charset="UTF-8" data-thmr="thmr_700" target="_self" novalidate="novalidate" style="outline: none;" thmr_curr="0"><div><div class="form-item form-type-textfield form-item-custom-search-blocks-form-1" data-thmr="thmr_692 thmr_690" style="outline: none;">
+      <form class="search-form" action="/suche" method="get" id="custom-search-blocks-form-1" accept-charset="UTF-8" data-thmr="thmr_700" target="_self" novalidate="novalidate" style="outline: none;" thmr_curr="0"><div><div class="form-item form-type-textfield form-item-custom-search-blocks-form-1" data-thmr="thmr_692 thmr_690" style="outline: none;">
   <label class="element-invisible" for="edit-custom-search-blocks-form-1--2" data-thmr="thmr_691">Search this site </label>
  <input title="Geben Sie die Begriffe ein, nach denen Sie suchen." class="custom-search-box form-text" placeholder="Suchwort" type="text" id="edit-custom-search-blocks-form-1--2" name="s" value="<?php print drupal_get_query_parameters()["s"]; ?>" size="15" maxlength="128" data-thmr="thmr_689" style="outline: none;">
 </div>
