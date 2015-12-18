a698afded6e85db040339678b9f70bc0d3fdb898
Boris Hekele <hekele@parliamentwatch.org>
2015-12-18 12:36:05 +0100

    fix header text issues

--- sites/all/themes/custom/parliamentwatch/templates/views-view-field--pw-testimonials-header--block--field-testimonial-fullname.tpl.php
+++ sites/all/themes/custom/parliamentwatch/templates/views-view-field--pw-testimonials-header--block--field-testimonial-fullname.tpl.php
@@ -1,3 +1,3 @@
 <div class="small light views-field-field-testimonial-fullname">
-  <?php print $output; ?> <span class="desktop-only inline"> ist eines von <span id="mscount">%count%</span> Förderinnen und Förderern von abgeordnetenwatch.de.</span>
+  <?php print $output; ?> <span class="desktop-only inline"> ist eines von <span id="mscount">%count%</span> Fördermitgliedern von abgeordnetenwatch.de</span>
 </div>
