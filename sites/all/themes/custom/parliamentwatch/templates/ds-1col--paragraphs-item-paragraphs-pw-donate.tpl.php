35c557528f0b3c92f5d4c08ccbc77581b4d3099b
Boris Hekele <hekele@parliamentwatch.org>
2016-03-29 17:06:43 +0200

    AW-3224 AW-3223 several fixes for the newsletter

--- sites/all/themes/custom/parliamentwatch/templates/ds-1col--paragraphs-item-paragraphs-pw-donate.tpl.php
+++ sites/all/themes/custom/parliamentwatch/templates/ds-1col--paragraphs-item-paragraphs-pw-donate.tpl.php
@@ -1,10 +1,10 @@
-<?php $node_url=url('node/10508', array('absolute' => TRUE)); ?>
+<?php $content_link_url = $content['field_pg_content_link'][0]['#element']['url']; ?>
 <div class="relative entity-paragraphs-item <?php print render($content['field_pg_donate_targetgroup']); ?>">
   <div class="social-media">
-    <a href="https://www.facebook.com/sharer/sharer.php?u=<?php print $node_url; ?>" class="facebook" target="_blank">facebook</a>
-    <a href="https://twitter.com/intent/tweet?text=Jetzt für abgeordnetenwatch.de spenden&url=<?php print $node_url; ?>" class="twitter" target="_blank">twitter</a>
+    <a href="https://www.facebook.com/sharer/sharer.php?u=<?php print $content_link_url; ?>" class="facebook" target="_blank">facebook</a>
+    <a href="https://twitter.com/intent/tweet?text=Jetzt für abgeordnetenwatch.de spenden&url=<?php print $content_link_url; ?>" class="twitter" target="_blank">twitter</a>
   </div>
-  <h3><a href="<?php print $node_url; ?>"><?php print render($content['field_pg_content_title']); ?></a></h3>
+  <h3><a href="<?php print $content_link_url; ?>"><?php print render($content['field_pg_content_title']); ?></a></h3>
   <div class="push-bottom-s">
     <?php print render($content['field_pg_content_body']); ?>
   </div>
--- sites/all/themes/custom/parliamentwatch/templates/ds-1col--paragraphs-item-paragraphs-pw-testmonial.tpl.php
+++ sites/all/themes/custom/parliamentwatch/templates/ds-1col--paragraphs-item-paragraphs-pw-testmonial.tpl.php
@@ -1,9 +1,8 @@
-<?php $node_url=url('node/10508', array('absolute' => TRUE)); ?>
 <div class="node-testimonial eyecatcher relative <?php print render($content['field_pg_donate_targetgroup']); ?>">
   <div class="social-media">
-    <a href="https://www.facebook.com/sharer/sharer.php?u=<?php print $node_url; ?>" class="facebook" target="_blank">facebook</a>
-    <a href="https://twitter.com/intent/tweet?text=Jetzt Fördermitglied von abgeordnetenwatch.de werden&url=<?php print $node_url; ?>" class="twitter" target="_blank">twitter</a>
+    <a href="https://www.facebook.com/sharer/sharer.php?u=<?php print render($content['field_pg_content_link']); ?>" class="facebook" target="_blank">facebook</a>
+    <a href="https://twitter.com/intent/tweet?text=Jetzt Fördermitglied von abgeordnetenwatch.de werden&url=<?php print render($content['field_pg_content_link']); ?>" class="twitter" target="_blank">twitter</a>
   </div>
   <?php print render($content['field_pg_testimonial_er']); ?>
-  <div class="text-right"><a href="<?php print $node_url; ?>">Jetzt Fördermitglied werden <span class="icon-thanks aw-icon-2x"></span></a></div>
+  <div class="text-right"><a href="<?php print render($content['field_pg_content_link']); ?>">Jetzt Fördermitglied werden <span class="icon-thanks aw-icon-2x"></span></a></div>
 </div>
