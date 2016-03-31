09e96d24e5cd1b0ac10291deae97a47f9373b8a0
Boris Hekele <hekele@parliamentwatch.org>
2016-03-31 13:44:46 +0200

    AW-3080 fix excluded answers

--- sites/all/modules/custom/pw_dialogues/pw_dialogues.js
+++ sites/all/modules/custom/pw_dialogues/pw_dialogues.js
@@ -1,7 +1,7 @@
 (function ($) {
     Drupal.behaviors.pw_dialogues = {
         attach: function () {
-            $('.user-profile .facetapi-facetapi-checkbox-links a').each(function () {
+            $('.user-profile .facetapi-facetapi-links a').each(function () {
                $(this).attr('href', $(this).attr('href') + '#pw-block-questions-and-answers');
             });
             $('.block-pw-dialogues-qa .pager li a').each(function () {
