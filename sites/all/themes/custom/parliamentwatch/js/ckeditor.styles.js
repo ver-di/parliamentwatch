edffbc61f7b2692a5d4ceec826e2a7e23c3cce0c
Boris Hekele <hekele@parliamentwatch.org>
2016-05-02 20:05:02 +0200

    AW-3299: Fixed missing template definitions for ckeditor (update persistent); Added new template-icons;

--- sites/all/themes/custom/parliamentwatch/js/ckeditor.styles.js
+++ sites/all/themes/custom/parliamentwatch/js/ckeditor.styles.js
@@ -8,6 +8,7 @@ For licensing, see LICENSE.html or http://ckeditor.com/license
  * The 'Styles' button is not enabled by default in DrupalFull and DrupalFiltered toolbars.
  */
 if(typeof(CKEDITOR) !== 'undefined') {
+    CKEDITOR.config.templates = 'custom';
     CKEDITOR.addStylesSet( 'drupal',
     [
             /* Block Styles */
@@ -16,30 +17,30 @@ if(typeof(CKEDITOR) !== 'undefined') {
             // not needed here by default. You may enable them to avoid placing the
             // "Format" drop-down list in the toolbar, maintaining the same features.
             /*
-            { name : 'Paragraph'		, element : 'p' },
-            { name : 'Heading 1'		, element : 'h1' },
-            { name : 'Heading 2'		, element : 'h2' },
-            { name : 'Heading 3'		, element : 'h3' },
-            { name : 'Heading 4'		, element : 'h4' },
-            { name : 'Heading 5'		, element : 'h5' },
-            { name : 'Heading 6'		, element : 'h6' },
+            { name : 'Paragraph'        , element : 'p' },
+            { name : 'Heading 1'        , element : 'h1' },
+            { name : 'Heading 2'        , element : 'h2' },
+            { name : 'Heading 3'        , element : 'h3' },
+            { name : 'Heading 4'        , element : 'h4' },
+            { name : 'Heading 5'        , element : 'h5' },
+            { name : 'Heading 6'        , element : 'h6' },
             { name : 'Preformatted Text', element : 'pre' },
-            { name : 'Address'			, element : 'address' },
+            { name : 'Address'          , element : 'address' },
 
-            { name : 'Blue Title'		, element : 'h3', styles : { 'color' : 'Blue' } },
-            { name : 'Red Title'		, element : 'h3', styles : { 'color' : 'Red' } },
+            { name : 'Blue Title'       , element : 'h3', styles : { 'color' : 'Blue' } },
+            { name : 'Red Title'        , element : 'h3', styles : { 'color' : 'Red' } },
 
             /* Inline Styles */
 
             // These are core styles available as toolbar buttons. You may opt enabling
             // some of them in the "Styles" drop-down list, removing them from the toolbar.
             /*
-            { name : 'Strong'			, element : 'strong', overrides : 'b' },
-            { name : 'Emphasis'			, element : 'em'	, overrides : 'i' },
-            { name : 'Underline'		, element : 'u' },
-            { name : 'Strikethrough'	, element : 'strike' },
-            { name : 'Subscript'		, element : 'sub' },
-            { name : 'Superscript'		, element : 'sup' },
+            { name : 'Strong'           , element : 'strong', overrides : 'b' },
+            { name : 'Emphasis'         , element : 'em'    , overrides : 'i' },
+            { name : 'Underline'        , element : 'u' },
+            { name : 'Strikethrough'    , element : 'strike' },
+            { name : 'Subscript'        , element : 'sub' },
+            { name : 'Superscript'      , element : 'sup' },
             */
             /* Text Styles */
 
@@ -83,4 +84,68 @@ if(typeof(CKEDITOR) !== 'undefined') {
                     }
             }
     ]);
+    // Register a templates definition set named "default".
+    CKEDITOR.addTemplates( 'custom', {
+        // The name of sub folder which hold the shortcut preview images of the
+        // templates.
+        imagesPath: CKEDITOR.getUrl( CKEDITOR.plugins.getPath( 'templates' ) + 'templates/images/' ),
+
+        // The templates definitions.
+        templates: [
+            {
+                title: 'Floatbox',
+                image: 'template_default_1.gif',
+                description: 'A floating box on the left with headline.',
+                html: '<div class="floatbox floatbox-left">' +
+                    '<h3>' +
+                        'Title goes here' +
+                    '</h3>' +
+                    '<h4>' +
+                            'Subtitle goes here' +
+                    '</h4>' +
+                    '<ul>' +
+                        '<li>List item 1</li>' +
+                        '<li>List item 2</li>' +
+                        '<li>List item 3</li>' +
+                    '</ul>' +
+                    '</div>'
+            },
+            {
+                title: 'Floatbox',
+                image: 'template_default_2.gif',
+                description: 'A floating box on the right with headline.',
+                html: '<div class="floatbox floatbox-right">' +
+                    '<h3>' +
+                        'Title goes here' +
+                    '</h3>' +
+                    '<h4>' +
+                            'Subtitle goes here' +
+                    '</h4>' +
+                    '<ul>' +
+                        '<li>List item 1</li>' +
+                        '<li>List item 2</li>' +
+                        '<li>List item 3</li>' +
+                    '</ul>' +
+                    '</div>'
+            },
+            {
+                title: 'Image container with ©opyright',
+                image: 'template_default_3.gif',
+                description: 'A container with an image and a subline floating to the left.',
+                html: '<div class="file-image float-left">' +
+                        '<img src="/sites/all/modules/contrib/ckeditor/ckeditor/plugins/templates/templates/images/template_default_3.gif">' +
+                        '<div class="copyright">©opyright</div>' +
+                    '</div>'
+            },
+            {
+                title: 'Image container with ©opyright',
+                image: 'template_default_4.gif',
+                description: 'A container with an image and a subline floating to the right.',
+                html: '<div class="file-image float-right">' +
+                        '<img src="/sites/all/modules/contrib/ckeditor/ckeditor/plugins/templates/templates/images/template_default_4.gif">' +
+                        '<div class="copyright">©opyright</div>' +
+                    '</div>'
+            }
+        ]
+    });
 }
