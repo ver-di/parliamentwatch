b7fb6628e1845ff133eca325afe5b88f68391e33
Boris Hekele <hekele@parliamentwatch.org>
2016-08-23 16:10:11 +0200

    modifications to custom modules

---
+++ sites/verdi/modules/custom/pw_dialogues/comment-placeholder.tpl.php
@@ -0,0 +1,40 @@
+<?php
+
+/**
+ * @file
+ * Default theme implementation for comments.
+ *
+ * Available variables:
+ * - $title: Title.
+ * - $classes: String of classes that can be used to style contextually through
+ *   CSS. It can be manipulated through the variable $classes_array from
+ *   preprocess functions. The default values can be one or more of the following:
+ *   - comment: The current template type, i.e., "theming hook".
+ * - $permalink: Comment permalink.
+ * - $picture: Authors picture.
+ * - $title_prefix (array): An array containing additional output populated by
+ *   modules, intended to be displayed in front of the main title tag that
+ *   appears in the template.
+ * - $title_suffix (array): An array containing additional output populated by
+ *   modules, intended to be displayed after the main title tag that appears in
+ *   the template.
+ *
+ * This variable is provided for context:
+ * - $node: Node object the comments are attached to.
+ *
+ * Other variables:
+ * - $classes_array: Array of html class attribute values. It is flattened
+ *   into a string within the variable $classes.
+ *
+ * @see template_preprocess()
+ * @see template_process()
+ *
+ * @ingroup themeable
+ */
+?>
+<div class="<?php print $classes; ?> clearfix"<?php print $attributes; ?>>
+  <?php print render($title_prefix); ?>
+  <h3<?php print $title_attributes; ?>><?php print $title ?></h3>
+  <?php print render($title_suffix); ?>
+  <?php print render($content['links']) ?>
+</div>
---
+++ sites/verdi/modules/custom/pw_dialogues/pw_dialogues.js
@@ -0,0 +1,12 @@
+(function ($) {
+    Drupal.behaviors.pw_dialogues = {
+        attach: function () {
+            $('.user-profile .facetapi-facetapi-links a').each(function () {
+               $(this).attr('href', $(this).attr('href') + '#pw-block-questions-and-answers');
+            });
+            $('.block-pw-dialogues-qa .pager li a').each(function () {
+               $(this).attr('href', $(this).attr('href') + '#pw-block-questions-and-answers');
+            });
+        }
+    };
+})(jQuery);
