9dc31788dde18964d2ea59bd967226d39205479c
Christopher Neugebauer <christopher.neugebauer.87@googlemail.com>
2016-11-28 02:20:04 +0100

    dialogues-detail styling

---
+++ sites/all/themes/custom/verdi/templates/node/ds-1col--node-dialogue-full.tpl.php
@@ -0,0 +1,15 @@
+<?php
+
+/**
+ * @file
+ * Display Suite 1 column template.
+ */
+?>
+<<?php print $ds_content_wrapper; print $layout_attributes; ?> id="question" class="question-detail <?php print $classes;?> clearfix">
+
+    <?php if (isset($title_suffix['contextual_links'])): ?>
+        <?php print render($title_suffix['contextual_links']); ?>
+    <?php endif; ?>
+
+    <?php print $ds_content; ?>
+</<?php print $ds_content_wrapper ?>>
