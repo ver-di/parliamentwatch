af4111df02b7f442df77aa2787dc6458302034c1
Boris Hekele <hekele@parliamentwatch.org>
2016-12-13 10:09:17 +0100

    VER-50 hide title on specific pages

--- sites/all/themes/custom/verdi/template.php
+++ sites/all/themes/custom/verdi/template.php
@@ -3,3 +3,11 @@
  * @file
  * The primary PHP file for this theme.
  */
+/*
+ * hide title
+ */
+function verdi_preprocess_page(&$variables) {
+  if(isset($variables['node']) && $variables['node']->type == 'dialogue' || isset($variables['user'])){
+    drupal_set_title('');
+  }
+}
