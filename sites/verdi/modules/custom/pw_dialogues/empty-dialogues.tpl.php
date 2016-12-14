68ae7c16c2d9d39721efd4c0ebdd7d532389a1b6
Boris Hekele <hekele@parliamentwatch.org>
2016-12-14 10:28:47 +0100

    placeholder if there are no questions

---
+++ sites/verdi/modules/custom/pw_dialogues/empty-dialogues.tpl.php
@@ -0,0 +1,8 @@
+<div class="padded">
+  <p class="text-center">Bisher wurde noch keine Frage eingereicht. <br />
+    <strong>Nutze jetzt die Möglichkeit dein Anliegen Gehör zu verleihen.</strong>
+  </p>
+  <p class="text-center">
+    <a href="/node/8?recipient=<?php print $recipient_name; ?>" class="btn btn-primary colorbox-node">Jetzt Frage stellen</a>
+  </p>
+</div>
