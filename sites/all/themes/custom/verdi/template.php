e0eaa17bd71fcb64dcb60f2216d64234f71061ff
Boris Hekele <hekele@parliamentwatch.org>
2016-12-14 19:54:15 +0100

    VER-50 fix of hiding title

--- sites/all/themes/custom/verdi/template.php
+++ sites/all/themes/custom/verdi/template.php
@@ -7,7 +7,7 @@
  * hide title
  */
 function verdi_preprocess_page(&$variables) {
-  if(isset($variables['node']) && $variables['node']->type == 'dialogue' || isset($variables['user'])){
+  if(isset($variables['node']) && $variables['node']->type == 'dialogue' || arg(0) == 'user'){
     drupal_set_title('');
   }
 }
